bef8159b41b584d88cf1eae33660e1f01d13acf3
nasrin <n.mokhtari@wiki-land.net>
2025-02-09 10:59:17 +0330

    fetch data with redux

--- movie/src/components/Home/Home.js
+++ movie/src/components/Home/Home.js
@@ -2,21 +2,23 @@ import React, { useEffect } from 'react'
 import MovieListing from "../MovieListing/MovieListing";
 import movieApi from "../../components/apis/movieApi";
 import {APIKey} from "../../components/apis/MovieApikey";
+import { useDispatch } from 'react-redux';
+import { addMovies } from '../../features/movies/movieSlice';
 
 function Home() {
-
-useEffect(() => {
   const movieText = "Harry";
-  const fetchMovies = async () => {
-    const response = await movieApi.get(`?APIKey=${APIKey}&s=${movieText}&type=movie`)
-    //type this for view data or go to Network tab >>>>  https://www.omdbapi.com/?APIKey=3d91bb7a&s=Harry&type=movie
-    .catch((err) => {
-      console.log("Err :", err);
-    });
-    console.log("THE response from API", response);
-  };
-  fetchMovies();
-},[]);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await movieApi.get(`?APIKey=${APIKey}&s=${movieText}&type=movie`)
+      //type this for view data or go to Network tab >>>>  https://www.omdbapi.com/?APIKey=3d91bb7a&s=Harry&type=movie
+      .catch((err) => {
+        console.log("Err :", err);
+      });
+      dispatch(addMovies(response.data));
+    };
+    fetchMovies();
+  },[]);
 
   return (
     <div>
